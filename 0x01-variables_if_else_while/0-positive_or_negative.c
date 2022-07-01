415206a3137022b67fecba3575e8450cc16b316c
emilykamiti <emilykamiti@gmail.com>
2022-07-01 11:59:46 -0700

    shit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,10 +4,11 @@
 /**
  * main -> assign to the variable
  * Return : always 0
- */ 
- int main(void)
+*/ 
+int main(void)
 {
-	int n;
+int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
