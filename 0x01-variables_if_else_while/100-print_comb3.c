44366ac2ede761a847ba5c0854e6f193763e5422
emilykamiti <emilykamiti@gmail.com>
2022-07-01 14:50:53 -0700

    my

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,18 +8,30 @@
  */
 int main(void)
 {
-int i = 0;
-int j = 0;
-for (i = 0; i < 10; i++)
-{
-for (j = i + 1; j < 10; j++)
-{
-putchar(47 + i);
-putchar(47 + j);
-putchar(',');
-putchar(' ');
-}
-}
-putchar ('\n');
-return (0);
+	int c;
+	int d = 0;
+
+	while (d < 10)
+	{
+		c = 0;
+		while (c < 10)
+		{
+			if (d != c && d < c)
+			{
+				putchar('0' + d);
+				putchar('0' + c);
+
+				if (c + d != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
 }
