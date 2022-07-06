248e2dd92112b05c702d43741a6d8f7903fbc59f
emilykamiti <emilykamiti@gmail.com>
2022-07-06 12:19:05 -0700

    Loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,17 @@
 #include "main.h"
-/* more headers goes there */
-/* Below is the function definition for a function that prints a to z */
-/* betty style doc for function main goes there */
+
 /**
- * main - Prints the last digit of a randomly generated number
- *        and whether it is greater than 5, less than 6, or 0.
+ * print_alphabet - print alphabet lowercase
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
-}
+	char alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++
+			{	
+			_putchar(alphabet);
+			}
+			_putchar('\n');
+			}
