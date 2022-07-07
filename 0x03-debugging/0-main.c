1ae216a0eaa274883f449aadd31ab25334e30705
emilykamiti <emilykamiti@gmail.com>
2022-07-07 11:53:59 -0700

    Debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,20 +1,16 @@
 #include "main.h"
+
 /**
- * main - main
- * Return: 0
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
 	int i;
 
 	i = 0;
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
->>>>>>> 6b73de60fe4613973a3da2fd82ea1887784cb58d
-=======
->>>>>>> 867941c40e4cb6b8ccefa7879f860205319e5ae5
 	positive_or_negative(i);
+
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,12 +2,10 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 void positive_or_negative(int i);
-<<<<<<< HEAD
-int main(void)
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-endif /* MAIN_H */
-=======
-
-#endif /* MAIN_H */
->>>>>>> 867941c40e4cb6b8ccefa7879f860205319e5ae5
+#endif /* MAIN_H */ 
