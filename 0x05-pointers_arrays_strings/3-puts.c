3ae41d7121ffe1b445eec402052bb4cb51bb6a8e
emilykamiti <emilykamiti@gmail.com>
2022-07-11 12:29:07 -0700

    MY Pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string followed by  new line to stdout
+ * @str: string to be rinted
+ */
+void _puts(char *str)
+{
+while (*str)
+{
+	_putchar(*str);
+	str++;
+}
+_putchar('\n');
+}
