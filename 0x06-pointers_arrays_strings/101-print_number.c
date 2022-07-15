6175fe1223b9f0407b9d94fdcac26ff147f5a2f4
emilykamiti <emilykamiti@gmail.com>
2022-07-15 13:29:25 -0700

    My pass

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -5,15 +5,15 @@
  *Return: nothing 
  */ 
 void print_number(int n) 
-{ 
-        unsigned int x = n; 
- 
-        if (n < 0) 
-        { 
-                _putchar('-'); 
-                x = -x; 
-        } 
-        if ((x / 10) > 0) 
-                print_number(x / 10); 
-        _putchar((x % 10) + '0'); 
+{
+	unsigned int x = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		x = -x;
+	}
+	if ((x / 10) > 0)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
