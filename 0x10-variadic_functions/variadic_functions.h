5f322e71353b62c761e97163c3aa8672d4df9e94
emilykamiti <emilykamiti@gmail.com>
2022-08-04 10:53:48 -0700

    name

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,6 @@
-#ifndef _VARIADIC_FUNCTIONS_
-#define _VARIADIC_FUNCTIONS_
+#ifndef VARFUNC
+#define VARFUNC
 #include <stdarg.h>
-#include <stdio.h>
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
@@ -15,7 +13,7 @@ void print_all(const char * const format, ...);
  */
 typedef struct typ
 {
-char *c;
-void (*tp)(va_list var);
+	char *c;
+	void (*tp)(va_list var);
 } typ_t;
 #endif
