c4c6149c817337961aefa360c14f69c8ebc903ec
emilykamiti <emilykamiti@gmail.com>
2022-08-23 07:43:38 -0700

    pass

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,11 +7,10 @@
 #include <stdlib.h>
 /**
  * read_textfile - Reads a text file and prints it to the POSIX
- * @letters: The number of letters the
- *           function should read and print.
- *
- * Return: If the function fails or filename is NULL - 0.
- * Developer: kamiti Emily
+ * standard output.
+ * @filename: file.
+ * @letters: Number of letters it should read and print.
+ * Return: Actual number of letters it could read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
