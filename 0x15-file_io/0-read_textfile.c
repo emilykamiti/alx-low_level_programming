99e627164650ec3b69e09725c3a578915bc68293
emilykamiti <emilykamiti@gmail.com>
2022-08-23 06:32:02 -0700

    Baby

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 
 /*
+ * main - check code
  * Desc: function that reads a textfile and prints
  *
  * Return : Always 0
@@ -10,15 +11,26 @@
 
 ssize_t read_textfile(const char *filename, size_t letters);
 {
-	int fd, sz;
-	char c = (char *) malloc(100, sizeof(char));
+	ssize_t f_open, f_read, f_write;
+	char *buf;
 
-	fd = open("my rite code", O_RDONLY);
-	if (fd < 0)
-	perror("r1"); exit(1);
+	if (filename == NULL)
+		return (0);
 
-	sz = read(fd, c, 4);
-	printf("read(% d, c, 4). returned %d bytes were read.\n", fd, sz);
-	c[sz] = '\0';
-	printf("name them: %s\n", c);
+	buf= malloc(sizeof(char) *letters);
+	if (buf == NULL)
+		return (0);
+
+	f_open = open(filename, O_RDONLY);
+	f_write = write(f_open, buf, letters);
+	f_read = read(STOUT_FILENO, BUF,f_read);
+
+	if (f_open == -1 || f_read == _1 || f_write == -1 || f_write != f_read)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+	return (f_write);
 }
