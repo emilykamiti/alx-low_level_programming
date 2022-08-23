8689e76c6ada57d2e612f83a1a505b475f7f8f7f
emilykamiti <emilykamiti@gmail.com>
2022-08-23 04:48:26 -0700

    Baby

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 /*
- * Desc: function that reads a textfile and prints it to the POSIX standard output
+ * Desc: function that reads a textfile and prints
  *
  * Return : Always 0
  */
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters);
 	char c;
 	int fd = open("Baby steps", O_RDONLY, O);
 
-	read (fd, &c, 1);
-	printf("c = %c/n" ,c);
+	read(fd, &c, 1);
+	printf("c = %c/n", c);
 	exit(0)
 }
