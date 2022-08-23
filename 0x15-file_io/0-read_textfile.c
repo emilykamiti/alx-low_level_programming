12f1e9de1dbd48f0b40bf7bac107ce4db410d99e
emilykamiti <emilykamiti@gmail.com>
2022-08-23 04:12:42 -0700

    Baby steps

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+/*
+ * Desc: function that reads a textfile and prints it to the POSIX standard output
+ *
+ * Return : Always 0
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters);
+{
+	char c;
+	int fd = open("Baby steps", O_RDONLY, O);
+
+	read (fd, &c, 1);
+	printf("c = %c/n" ,c);
+	exit(0)
+}
