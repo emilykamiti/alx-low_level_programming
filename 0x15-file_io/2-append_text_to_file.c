64a91e445f97fb55a8127aab2652e1dbcec7b112
emilykamiti <emilykamiti@gmail.com>
2022-08-24 10:56:52 -0700

    Append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,26 +12,28 @@
  * @text_content: NULL terminated string to write to the file.
  * Return: 1 on success, -1 on failure.
  */
-nt append_text_to_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int op, wr, len = 0;
+	int fd, res_write, lenght;
 
 	if (filename == NULL)
 		return (-1);
-
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[len];)
-			len++;
+		fd = open(filename, O_WRONLY | O_APPEND);
+		if (fd == -1)
+			return (-1);
+		lenght = 0;
+		while (*(text_content + lenght) != '\0')
+			lenght++;
+		res_write = write(fd, text_content, lenght);
+		if (res_write == -1)
+		{
+			close(fd);
+			write(STDOUT_FILENO, "fails", 5);
+			return (-1);
+		}
 	}
-
-	op = open(filename, O_WRONLY | O_APPEND);
-	wr = write(op, text_content, len);
-
-	if (op == -1 || wr == -1)
-		return (-1);
-
-	close(op);
-
+	close(fd);
 	return (1);
 }
